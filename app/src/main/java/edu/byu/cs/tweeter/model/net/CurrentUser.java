780aa318586db1eb4f36a7fb3df67a06bab79292
Heeyeon Lee <heeyoenl@byu.edu>
2021-02-23 13:04:13 -0700

    added CurrentUser in net

---
+++ app/src/main/java/edu/byu/cs/tweeter/model/net/CurrentUser.java
@@ -0,0 +1,12 @@
+package edu.byu.cs.tweeter.model.net;
+
+import edu.byu.cs.tweeter.model.domain.User;
+
+public class CurrentUser {
+
+    private static User currentUser;
+
+    public CurrentUser(User user) {
+
+    }
+}
