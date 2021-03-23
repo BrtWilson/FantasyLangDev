634ceb984b1ca98a5efd439346d52fac219dc3c9
DHICORP\bjensen <bjensen@dhicorp.com>
2021-03-23 09:13:42 -0600

    Fixed LogoutHAndler and created shadow jar

--- server/src/main/java/com/example/server/lambda/LogoutHandler.java
+++ server/src/main/java/com/example/server/lambda/LogoutHandler.java
@@ -4,16 +4,18 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.example.server.service.LoginService;
 import com.example.shared.model.service.request.LoginRequest;
+import com.example.shared.model.service.request.LogoutRequest;
+import com.example.shared.model.service.response.BasicResponse;
 import com.example.shared.model.service.response.LoginResponse;
 
 import java.io.IOException;
 
-public class LogoutHandler implements RequestHandler<LoginRequest, LoginResponse> {
+public class LogoutHandler implements RequestHandler<LogoutRequest, BasicResponse> {
     @Override
-    public LoginResponse handleRequest(LoginRequest loginRequest, Context context) {
+    public BasicResponse handleRequest(LogoutRequest logoutRequest, Context context) {
         LoginService loginService = new LoginService();
         try {
-            return loginService.logout(loginRequest);
+            return loginService.logout(logoutRequest);
         } catch (RuntimeException | IOException e) {
             String message = "[Bad Request]";
             throw new RuntimeException(message, e);
