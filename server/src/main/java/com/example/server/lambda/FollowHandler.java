d96ff1fb602a88ea077878ac5650ebfe476b1293
DHICORP\bjensen <bjensen@dhicorp.com>
2021-03-22 22:48:47 -0600

    removed Follow stuff i added

--- server/src/main/java/com/example/server/lambda/FollowHandler.java
+++ server/src/main/java/com/example/server/lambda/FollowHandler.java
@@ -1,24 +1,5 @@
 package com.example.server.lambda;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.example.server.service.FollowService;
-import com.example.shared.model.net.TweeterRemoteException;
-import com.example.shared.model.service.request.FollowRequest;
-import com.example.shared.model.service.response.FollowResponse;
+public class FollowHandler {
 
-import java.io.IOException;
-
-public class FollowHandler implements RequestHandler<FollowRequest, FollowResponse> {
-
-    @Override
-    public FollowResponse handleRequest(FollowRequest request, Context context) {
-        FollowService service = new FollowService();
-        try {
-            return service.followResponse(request);
-        }catch (RuntimeException | TweeterRemoteException |IOException e) {
-                String message = "[Bad Request]";
-                throw new RuntimeException(message, e);
-            }
-    }
 }
