656e5be92802abf7c2bd1a8fb6497ec3fd1a4071
DHICORP\bjensen <bjensen@dhicorp.com>
2021-03-23 09:10:29 -0600

    got num followers num followees get follow status follow and unfollow lambdas login

--- server/src/main/java/com/example/server/lambda/LogoutHandler.java
+++ server/src/main/java/com/example/server/lambda/LogoutHandler.java
@@ -1,4 +1,22 @@
 package com.example.server.lambda;
 
-public class LogoutHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.example.server.service.LoginService;
+import com.example.shared.model.service.request.LoginRequest;
+import com.example.shared.model.service.response.LoginResponse;
+
+import java.io.IOException;
+
+public class LogoutHandler implements RequestHandler<LoginRequest, LoginResponse> {
+    @Override
+    public LoginResponse handleRequest(LoginRequest loginRequest, Context context) {
+        LoginService loginService = new LoginService();
+        try {
+            return loginService.logout(loginRequest);
+        } catch (RuntimeException | IOException e) {
+            String message = "[Bad Request]";
+            throw new RuntimeException(message, e);
+        }
+    }
 }
