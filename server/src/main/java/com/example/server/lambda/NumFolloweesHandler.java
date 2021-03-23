5a75e8678f6a05c9bbc7372f0fdf253ebf9f65c7
DHICORP\bjensen <bjensen@dhicorp.com>
2021-03-23 09:09:40 -0600

    got num followers num followees get follow status follow and unfollow lambdas

--- server/src/main/java/com/example/server/lambda/NumFolloweesHandler.java
+++ server/src/main/java/com/example/server/lambda/NumFolloweesHandler.java
@@ -1,4 +1,32 @@
 package com.example.server.lambda;
 
-public class NumFolloweesHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.example.server.service.FollowingService;
+import com.example.shared.model.service.request.FollowingRequest;
+import com.example.shared.model.service.response.FollowingResponse;
+
+import java.io.IOException;
+
+public class NumFolloweesHandler implements RequestHandler<FollowingRequest, FollowingResponse> {
+
+    /**
+     * Returns the users that the user specified in the request is following. Uses information in
+     * the request object to limit the number of followees returned and to return the next set of
+     * followees after any that were returned in a previous request.
+     *
+     * @param request contains the data required to fulfill the request.
+     * @param context the lambda context.
+     * @return the followees.
+     */
+    @Override
+    public FollowingResponse handleRequest(FollowingRequest request, Context context) {
+        FollowingService service = new FollowingService();
+        try {
+            return service.getNumFollowing(request);
+        } catch (RuntimeException | IOException e) {
+            String message = "[Bad Request]";
+            throw new RuntimeException(message, e);
+        }
+    }
 }
--- server/src/main/java/com/example/server/lambda/UnfollowHandler.java
+++ server/src/main/java/com/example/server/lambda/UnfollowHandler.java
@@ -1,4 +1,23 @@
 package com.example.server.lambda;
 
-public class UnfollowHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.example.server.service.FollowStatusService;
+import com.example.shared.model.service.request.FollowStatusRequest;
+import com.example.shared.model.service.response.FollowStatusResponse;
+
+import java.io.IOException;
+
+public class UnfollowHandler implements RequestHandler<FollowStatusRequest, FollowStatusResponse> {
+
+    @Override
+    public FollowStatusResponse handleRequest(FollowStatusRequest request, Context context) {
+        FollowStatusService service = new FollowStatusService();
+        try {
+            return service.unfollow(request);
+        }catch (RuntimeException | IOException e) {
+            String message = "[Bad Request]";
+            throw new RuntimeException(message, e);
+        }
+    }
 }
