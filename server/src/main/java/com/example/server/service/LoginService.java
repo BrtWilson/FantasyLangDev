9596a37788f4d52c29f6f382eff5e08dbf8b5fd0
James Belnap <james6bcdn@gmail.com>
2021-04-13 22:16:43 -0600

    re-hash

--- server/src/main/java/com/example/server/service/LoginService.java
+++ server/src/main/java/com/example/server/service/LoginService.java
@@ -23,7 +23,7 @@ public class LoginService implements ILoginService {
     public LoginResponse login(LoginRequest request) throws IOException {
         PasswordHasher passwordHasher = new PasswordHasher();
         String hashedPassword = passwordHasher.hashPassword(request.getPassword());
-        //request.setPassword(hashedPassword);
+        request.setPassword(hashedPassword);
 
         return getLoginDao().login(request);
     }
